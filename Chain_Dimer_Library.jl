d9579fe9807ff99980f97aefcc9454eb1104da58
Aleksandr Rodin <alerodin@gmail.com>
2019-08-30 11:43:29 +0800

    Fixed Lambda vs Lambda_Inv. Added comments.

--- Chain_Dimer_Library.jl
+++ Chain_Dimer_Library.jl
@@ -35,39 +35,43 @@ function Ξ(z, m :: Int)
     return p * ((M - √(M + 1) * √(M - 1))^abs.(m)) / ( √(M + 1) * √(M - 1))
 end
 
-# Scattering matrix Λ
-function Λ(z, ImpsT :: Array{Impurity, 1})
-    posT = map(x -> x.l, ImpsT)
-    ϵsT = map(x -> x.ϵ, ImpsT)
-    VsT = map(x -> x.V, ImpsT)
+# Inverse of the full impurity Green's function
+function Λ_Inv(z, ImpsT :: Array{Impurity, 1})
+    posT = map(x -> x.l, ImpsT) # Array of impurity positions
+    ϵsT = map(x -> x.ϵ, ImpsT)  # Array of impurity on-site energies
+    VsT = map(x -> x.V, ImpsT)  # Array of impurity-chain coupling energies
 
-    nImps = length(ImpsT)
+    nImps = length(ImpsT)       # Number of impurities
 
-    posT_Mat = repeat(posT, 1, nImps)
-    pos_Mat = transpose(posT_Mat)
+    posT_Mat = repeat(posT, 1, nImps)   # Impurity position matrix
+    pos_Mat = transpose(posT_Mat)       # Transpose of impurity position matrix
 
+    # Pairwise propagator between the impurity sites
     Prop = (map((x, y) -> Ξ(z, x - y), posT_Mat, pos_Mat))
 
-    V = Diagonal(VsT)
-    ϵ = Diagonal(ϵsT)
+    V = Diagonal(VsT)   # Diagonal of coupling energies
+    ϵ = Diagonal(ϵsT)   # Diagonal of on-site energies
 
+    # Inverse isolate impurity Green's function (matrix form)
     Γ_Inv = z .* Matrix{Int}(I, nImps, nImps) .- ϵ
 
+    # Return the full impurity Green's function
     return (Γ_Inv .- V * Prop * V)
 end
 
 # Integrand used to calculate the local density
 function Δρ_Integrand(r, z, Imps)
-    Λ_Inv = inv(Λ(z, Imps))
+    Λ = inv(Λ_Inv(z, Imps))
 
-    Vs = map(x -> x.V, Imps)
-    pos = map(x -> x.l, Imps)
+    Vs = map(x -> x.V, Imps)    # Array of impurity-chain coupling energies
+    pos = map(x -> x.l, Imps)   # Array of impurity positions
 
-    V = Diagonal(Vs)
+    V = Diagonal(Vs)            # Diagonal of coupling energies
 
+    # Propagator vector between point r and the impurity positions
     PropVector = reshape(map(x -> Ξ(z, r - x), pos), (length(Imps), 1))
 
-    return (transpose(PropVector) * V * Λ_Inv * V * PropVector)
+    return (transpose(PropVector) * V * Λ * V * PropVector)
 end
 
 # Local density function
@@ -79,13 +83,13 @@ end
 
 # Integrand used to compute the interaction energy
 function F_I_Integrand(z, Imps)
-    Λ_ = Λ(z, Imps)
+    Λ_Inv_ = Λ_Inv(z, Imps)
 
-    Vs = map(x -> x.V, Imps)
-    ϵs = map(x -> x.ϵ, Imps)
-    Ξ0 = Ξ(z, 0)
+    Vs = map(x -> x.V, Imps)    # Array of V
+    ϵs = map(x -> x.ϵ, Imps)    # Array of ϵ
+    Ξ0 = Ξ(z, 0)                # 0-distance propagator. Same for all impurities
 
-    return (-log(det(Diagonal(1 ./ (z .- ϵs - Ξ0 .* Vs .* Vs)) * Λ_)) / (2 * π))
+    return (-log(det(Diagonal(1 ./ (z .- ϵs - Ξ0 .* Vs .* Vs)) * Λ_Inv_)) / (2 * π))
 end
 
 # Impurity interaction energy
